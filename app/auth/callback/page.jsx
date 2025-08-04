8c5907fb5a7cad374d13f757dba015979c4e4746
anh-it2 <vuminhanhnamtruc2004@gmail.com>
2025-08-04 17:29:01 +0700

    update

--- app/auth/callback/page.jsx
+++ app/auth/callback/page.jsx
@@ -1,3 +1,26 @@
+// 'use client'
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+// const CallbackPage = () => {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const token = urlParams.get("jwtToken");
+
+//     if (token) {
+//       localStorage.setItem("jwtToken", token);
+//       router.replace('/dashboard')
+//     } else {
+//       console.error("JWT token not found in URL");
+//     }
+//   }, [router]);
+
+//   return <div>Processing login...</div>;
+// };
+
+// export default CallbackPage;
 'use client'
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
